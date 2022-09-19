2fa7e0ae1969e05f5eee72a02392342d9f256ddd
0384359 <0384359@schoolsnet.act.edu.au>
2022-09-20 06:25:03 +1000

    bad guy

---
+++ Enemy.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+onready var nav = get_parent()
+var path = []
+var path_node = 0
+var speed = 10
+onready var player = $"../../Player"
+
+
+
+
+
+func _physics_process(delta):
+	if path_node < path.size():
+		var direction = (path[path_node] - global_transform.origin)
+		if direction.length() < 1:
+			path_node +=1
+		else:
+			move_and_slide(direction.normalized() * speed, Vector3.UP)
+			
+func move_to(target_pos):
+	path = nav.get_simple_path(global_transform.origin, target_pos)
+	path_node = 0
+
+func _on_Timer_timeout():
+	move_to(player.global_transform.origin)
