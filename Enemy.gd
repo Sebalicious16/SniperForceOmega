721934d839d474439a2cea5b03dde023da76dd86
0384359 <0384359@schoolsnet.act.edu.au>
2022-10-31 00:13:08 +1100

    player detection

--- Enemy.gd
+++ Enemy.gd
@@ -9,13 +9,13 @@ func _ready():
 	
 func _process(delta):
 	if target:
-		var result = space_state. intersect_ray(global_transform.origin, target.global_transform.origin)
+		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
 		if result.collider.is_in_group("Player"):
 			look_at(target.global_transform.origin, Vector3.UP) 
 			move_to_target(delta)
 
 func _on_Area_body_entered(body): 
-	if body.is_in_group("player"):
+	if body.is_in_group("Player"):
 		target = body 
 		print(body.name + "entered")
 
@@ -25,4 +25,6 @@ func _on_Area_body_exited(body):
 		print(body.name + "exited")
 
 func move_to_target(delta):
-	var direction = (target.transform.origin - transform .origin).normalized()
+	var direction = (target.transform.origin - transform.origin).normalized()
+	move_and_slide(direction * speed * delta, Vector3.UP)
+	print ("move")
